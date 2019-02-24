b35ede3f6d2d836ac08a839f9c117ba49bcbb894
FrankScarlet <hferfrank@gmail.com>
2019-02-24 23:45:48 +0800

    第一次整歌词

---
+++ ass&lrc/lrcparser.py
@@ -0,0 +1,18 @@
+import urllib3
+import json
+import certifi
+
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+field_type = 'lyric'
+field_id = '1334297179'
+r = http.request('GET', 'https://api.imjad.cn/cloudmusic/', fields={'type': field_type, 'id': field_id})
+
+a = json.loads(r.data)
+b = a['lrc']['lyric']
+lines = b.splitlines()
+
+with open('d_zeal.lrc', 'w+') as f:
+    for line in lines:
+        f.write(line+'\n')
+
+# https://zhuanlan.zhihu.com/p/30246788
